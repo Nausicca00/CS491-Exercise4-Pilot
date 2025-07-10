f4186324f53f1b81387b399af9bff83246766bbd
Cory Bateman <coryb342@gmail.com>
2025-07-10 16:01:33 -0700

    Add the get and put methods to the backend

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'));
+
+app.get('/token', (req, res) => {
+  res.sendFile(path.join(__dirname, 'token.json'))
+})
+
+app.put('/token', (req, res) => {
+  const updated_token = req.body;
+  fs.writeFile(path.join(__dirname, 'token.json'), JSON.stringify(updated_token), (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.status(200).send('Token updated successfully');
+  });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
