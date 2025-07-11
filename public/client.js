018fa295abd5958ae897f3ec87b31e7c487bf2df
Cory Bateman <coryb342@gmail.com>
2025-07-11 11:28:42 -0700

    Add function to compare the client and server tokens

--- public/client.js
+++ public/client.js
@@ -1,16 +1,26 @@
-let token = {};
 const username = 'user' + ':' + Math.random();
 const browser_name = getBrowserName();
+let polling = null;
+
+/**
+ * @typedef {Object} Token
+ * @property {string} username - The username of the user.
+ * @property {string} browser - The name of the browser.
+ */
+const token = {
+  username: username,
+  browser: browser_name,
+}
 
 async function getToken() {
   try {
     const response = await fetch('/token');
     if (!response.ok) throw new Error('Could not get Token');
-    token = await response.json();
-    return token;
+    let server_token = await response.json();
+    return server_token;
   } catch (error) {
-    console.error('Error getting Token:', error);
-    throw error;
+      console.error('Error getting Token:', error);
+      throw error;
   }
 }
 
@@ -38,4 +48,16 @@ function getBrowserName() {
     if (userAgent.includes('Safari')) return 'Safari';
     if (userAgent.includes('Edge')) return 'Edge';
     return 'Unknown Browser';
+}
+
+async function compareTokens() {
+  try {
+    let server_token = await getToken();
+    if (server_token !== token) {
+      ping_button.disabled = false;
+      clearInterval(polling);
+    }
+  } catch (error) {
+      console.error('Error retrieving Token:', error);
+  }
 }
